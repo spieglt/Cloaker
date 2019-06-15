4f52d1e7e42141472b10267a2909ce2e169c533f
Theron <tspiegl@gmail.com>
2019-06-15 18:15:14 -0500

    version number

--- unix_gui/cloaker/mainwindow.cpp
+++ unix_gui/cloaker/mainwindow.cpp
@@ -18,7 +18,7 @@ MainWindow::~MainWindow()
 void MainWindow::on_actionAbout_Cloaker_triggered()
 {
     QMessageBox::about(this, "About Cloaker",
-                       "<h2>Cloaker v1.0</h2>"
+                       "<h2>Cloaker v1.1</h2>"
                        "<p>Copyright (C) Theron Spiegl 2019</p>"
                        "<p>Licensed under the GNU General Public License v3.0</p>"
                        "<p>WARNING: if you encrypt a file and lose or forget the password, the file cannot be recovered.</p>"
